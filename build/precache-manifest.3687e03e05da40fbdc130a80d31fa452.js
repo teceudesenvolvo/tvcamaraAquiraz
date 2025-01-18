bb150a2762ae7de18a1ef7f5f9c45c7d0617d709
teceudesenvolvo <79342596+teceudesenvolvo@users.noreply.github.com>
2025-01-18 17:37:50 -0300

    folder-build

---
+++ build/precache-manifest.3687e03e05da40fbdc130a80d31fa452.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9685096dcd781b2b5a022163317a4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a798a727ab401aec456b",
+    "url": "/static/css/main.cf1b523c.chunk.css"
+  },
+  {
+    "revision": "759a98be45a131f573a9",
+    "url": "/static/js/2.eb869738.chunk.js"
+  },
+  {
+    "revision": "a798a727ab401aec456b",
+    "url": "/static/js/main.061fc2db.chunk.js"
+  },
+  {
+    "revision": "8307d2d51f600537d84c",
+    "url": "/static/js/runtime~main.b7376b8d.js"
+  },
+  {
+    "revision": "313d0746e5f222fafe77b190d8f63664",
+    "url": "/static/media/BANNER.313d0746.png"
+  },
+  {
+    "revision": "5731b8fd9d69fcb3b85b0214ee83fcb0",
+    "url": "/static/media/LOGO-MEUHALL-SF.5731b8fd.png"
+  },
+  {
+    "revision": "349bd27cf2e5b3dc34c07860f92b34d9",
+    "url": "/static/media/cliente.349bd27c.jpg"
+  },
+  {
+    "revision": "b39af1b6a04b638f4d2b31c0e72f7b70",
+    "url": "/static/media/curso-bg-1.b39af1b6.png"
+  },
+  {
+    "revision": "92d77844acc045163f5ef6f161ad2f81",
+    "url": "/static/media/for-courses.92d77844.png"
+  },
+  {
+    "revision": "3319b78b5f5d23987f62eb40d63b8435",
+    "url": "/static/media/gerencia-e-controle.3319b78b.png"
+  },
+  {
+    "revision": "4ae7747a6365b092947742a5b958e5f9",
+    "url": "/static/media/logo.4ae7747a.png"
+  },
+  {
+    "revision": "4ae7747a6365b092947742a5b958e5f9",
+    "url": "/static/media/logo12.4ae7747a.png"
+  }
+]);
