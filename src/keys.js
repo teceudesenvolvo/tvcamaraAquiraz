bf2f5779af0713914eb4ce85c9bed8680e6b454a
teceudesenvolvo <79342596+teceudesenvolvo@users.noreply.github.com>
2025-01-18 18:07:07 -0300

    src-up

---
+++ src/keys.js
@@ -0,0 +1,5 @@
+
+export const PAGAR_ME_API_KEY = 'ak_test_qN9jJiIstFsLytsqip6LADfKsUqahH'
+export const PAGAR_ME_EK_KEY = 'ek_test_9zOLENmPDYtH8Gkn3EWUNprhSCPAck'
+export const ID_PLAN = '548587'
+export const BASE_URL = 'https://api.pagar.me/1/subscriptions'
